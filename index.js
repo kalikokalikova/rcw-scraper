96cbb168887aab49a18857feb88467fdcf8db62b
kalikokalikova <calico.goodrich@gmail.com>
2022-09-27 16:29:25 -0700

    add routes to show scraped data in browser at /results

--- index.js
+++ index.js
@@ -4,14 +4,25 @@ const cheerio = require('cheerio')
 const express = require('express')
 
 const app = express()
-
 const url = "https://apps.leg.wa.gov/rcw/default.aspx?cite=69.50.4013"
-axios(url)
-    .then(response => {
-        const html = response.data
-        const $ = cheerio.load(html)
-        const rcw = $('#contentWrapper').text()
-        console.log(rcw)
-    }).catch(err => console.log(err))
+
+
+app.get('/', function(req, res) {
+    res.json("This is my web scraper")
+})
+
+app.get('/results', (req, res) => {
+    axios(url)
+        .then(response => {
+            const html = response.data
+            const $ = cheerio.load(html)
+            const rcw = $('#contentWrapper').text()
+            // console.log(rcw)
+            res.json(rcw)
+        }).catch(err => console.log(err))
+
+})
+
+
 
 app.listen(PORT, () => console.log(`serving running on PORT ${PORT}`))
